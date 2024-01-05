b2ffc17e563275402dfb4ecac382cfd10e2e4bc7
shourya-r <rajputshourya7@gmail.com>
2024-01-05 18:03:38 +0530

    Added references to HTML elements

---
+++ index.js
@@ -0,0 +1,4 @@
+// Getting references to list, input box and button
+const list = document.querySelector('ul');
+const inputBox = document.querySelector('input');
+const addItemButton = document.querySelector('.add-item');
