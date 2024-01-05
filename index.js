8d6eb6af13d7920ed2247b9b9fe419fbbcf15720
shourya-r <rajputshourya7@gmail.com>
2024-01-05 18:14:56 +0530

    Started implementing the add Element function

--- index.js
+++ index.js
@@ -2,3 +2,16 @@
 const list = document.querySelector('ul');
 const inputBox = document.querySelector('input');
 const addItemButton = document.querySelector('.add-item');
+
+addItemButton.addEventListener('click', ()=>{
+    // getting item name from the inputBox
+    let item = inputBox.value;
+    //setting inputBox value to empty
+    inputBox.value = '';
+
+    // creating elements for list item
+    const listElement = document.createElement('li');
+    const itemName = document.createElement('span');
+    const deleteButton = document.createElement('button');
+
+})
